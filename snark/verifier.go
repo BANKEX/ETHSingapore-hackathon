6ff46b9cb961b2cd06f35518c2ddb477dd089334
EnoRage <enormousrage@gmail.com>
2018-12-08 05:04:53 +0800

    add verifier.go

---
+++ snark/verifier.go
@@ -0,0 +1,215 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package verifier
+
+import (
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = abi.U256
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+)
+
+// VerifierABI is the input ABI used to generate the binding from.
+const VerifierABI = "[{\"constant\":true,\"inputs\":[],\"name\":\"ScalarField\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"pure\",\"type\":\"function\"}]"
+
+// VerifierBin is the compiled bytecode used for deploying new contracts.
+const VerifierBin = `0x60c261002f600b82828239805160001a6073146000811461001f57610021565bfe5b5030600052607381538281f300730000000000000000000000000000000000000000301460806040526004361060555763ffffffff7c0100000000000000000000000000000000000000000000000000000000600035041663daad1e638114605a575b600080fd5b60606072565b60408051918252519081900360200190f35b7f30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001905600a165627a7a72305820037cd1ab680975951954ade38137cd0b4925423fedbf22083591d9155bf989c30029`
+
+// DeployVerifier deploys a new Ethereum contract, binding an instance of Verifier to it.
+func DeployVerifier(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Verifier, error) {
+	parsed, err := abi.JSON(strings.NewReader(VerifierABI))
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	address, tx, contract, err := bind.DeployContract(auth, parsed, common.FromHex(VerifierBin), backend)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &Verifier{VerifierCaller: VerifierCaller{contract: contract}, VerifierTransactor: VerifierTransactor{contract: contract}, VerifierFilterer: VerifierFilterer{contract: contract}}, nil
+}
+
+// Verifier is an auto generated Go binding around an Ethereum contract.
+type Verifier struct {
+	VerifierCaller     // Read-only binding to the contract
+	VerifierTransactor // Write-only binding to the contract
+	VerifierFilterer   // Log filterer for contract events
+}
+
+// VerifierCaller is an auto generated read-only Go binding around an Ethereum contract.
+type VerifierCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// VerifierTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type VerifierTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// VerifierFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type VerifierFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// VerifierSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type VerifierSession struct {
+	Contract     *Verifier         // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// VerifierCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type VerifierCallerSession struct {
+	Contract *VerifierCaller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts   // Call options to use throughout this session
+}
+
+// VerifierTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type VerifierTransactorSession struct {
+	Contract     *VerifierTransactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts   // Transaction auth options to use throughout this session
+}
+
+// VerifierRaw is an auto generated low-level Go binding around an Ethereum contract.
+type VerifierRaw struct {
+	Contract *Verifier // Generic contract binding to access the raw methods on
+}
+
+// VerifierCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type VerifierCallerRaw struct {
+	Contract *VerifierCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// VerifierTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type VerifierTransactorRaw struct {
+	Contract *VerifierTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewVerifier creates a new instance of Verifier, bound to a specific deployed contract.
+func NewVerifier(address common.Address, backend bind.ContractBackend) (*Verifier, error) {
+	contract, err := bindVerifier(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Verifier{VerifierCaller: VerifierCaller{contract: contract}, VerifierTransactor: VerifierTransactor{contract: contract}, VerifierFilterer: VerifierFilterer{contract: contract}}, nil
+}
+
+// NewVerifierCaller creates a new read-only instance of Verifier, bound to a specific deployed contract.
+func NewVerifierCaller(address common.Address, caller bind.ContractCaller) (*VerifierCaller, error) {
+	contract, err := bindVerifier(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &VerifierCaller{contract: contract}, nil
+}
+
+// NewVerifierTransactor creates a new write-only instance of Verifier, bound to a specific deployed contract.
+func NewVerifierTransactor(address common.Address, transactor bind.ContractTransactor) (*VerifierTransactor, error) {
+	contract, err := bindVerifier(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &VerifierTransactor{contract: contract}, nil
+}
+
+// NewVerifierFilterer creates a new log filterer instance of Verifier, bound to a specific deployed contract.
+func NewVerifierFilterer(address common.Address, filterer bind.ContractFilterer) (*VerifierFilterer, error) {
+	contract, err := bindVerifier(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &VerifierFilterer{contract: contract}, nil
+}
+
+// bindVerifier binds a generic wrapper to an already deployed contract.
+func bindVerifier(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(VerifierABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Verifier *VerifierRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Verifier.Contract.VerifierCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Verifier *VerifierRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Verifier.Contract.VerifierTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Verifier *VerifierRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Verifier.Contract.VerifierTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Verifier *VerifierCallerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Verifier.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Verifier *VerifierTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Verifier.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Verifier *VerifierTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Verifier.Contract.contract.Transact(opts, method, params...)
+}
+
+// ScalarField is a free data retrieval call binding the contract method 0xdaad1e63.
+//
+// Solidity: function ScalarField() constant returns(uint256)
+func (_Verifier *VerifierCaller) ScalarField(opts *bind.CallOpts) (*big.Int, error) {
+	var (
+		ret0 = new(*big.Int)
+	)
+	out := ret0
+	err := _Verifier.contract.Call(opts, out, "ScalarField")
+	return *ret0, err
+}
+
+// ScalarField is a free data retrieval call binding the contract method 0xdaad1e63.
+//
+// Solidity: function ScalarField() constant returns(uint256)
+func (_Verifier *VerifierSession) ScalarField() (*big.Int, error) {
+	return _Verifier.Contract.ScalarField(&_Verifier.CallOpts)
+}
+
+// ScalarField is a free data retrieval call binding the contract method 0xdaad1e63.
+//
+// Solidity: function ScalarField() constant returns(uint256)
+func (_Verifier *VerifierCallerSession) ScalarField() (*big.Int, error) {
+	return _Verifier.Contract.ScalarField(&_Verifier.CallOpts)
+}
