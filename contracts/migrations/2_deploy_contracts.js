70f53fd9eae7172424b5d51da727fe12d43bf4f7
Anton Bukov <k06aaa@gmail.com>
2018-12-08 05:38:00 +0800

    Add ordered list of intervals

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 // const BankexPlasma = artifacts.require('BankexPlasma');
 
 module.exports = async function (deployer) {
-    // deployer.deploy(BankexPlasma);
+  // deployer.deploy(BankexPlasma);
 };
