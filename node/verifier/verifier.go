23a16bcc20a5b7f3b7dfd14260bf31ba3c15ee69
EnoRage <enormousrage@gmail.com>
2018-12-08 07:19:45 +0800

    fix

--- node/verifier/verifier.go
+++ node/verifier/verifier.go
@@ -4,10 +4,6 @@ import "./server"
 
 func main() {
 
-
-
-	//ethClient.InitClient(conf.Geth_host)
-
 	//go listeners.Checker()
 	//go balance.UpdateBalance(&storage.Balance, conf.Plasma_contract_address)
 	//go event.Start(storage.Client, conf.Plasma_contract_address, &storage.Who, &storage.Amount, &storage.EventBlockHash, &storage.EventBlockNumber)
