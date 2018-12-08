62d565733ac21d29f862c3d462b151d6bef9388a
EnoRage <enormousrage@gmail.com>
2018-12-08 19:54:06 +0800

    add new events

--- node/ethereum/events/events.go
+++ node/ethereum/events/events.go
@@ -1,71 +1,132 @@
 package events
 
 import (
-	"../../config"
 	"../plasmacontract"
 	"context"
+	"fmt"
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 	"math/big"
+	"strconv"
 	"strings"
+	"time"
 )
 
-func ListenDeposit() {
+type EventCoinDeposited struct {
+	Who         common.Address `json:who`
+	Amount      *big.Int       `json:amount`
+	BlockNumber uint64         `json:blockNumber`
+}
 
-	client, err := ethclient.Dial(config.GetVerifier().GethHost)
+var eventGroup = make([]EventCoinDeposited, 0)
+var currentBlock uint64 = 0
+
+func GetEvent() bool {
+	client, err := ethclient.Dial("http://localhost:9545")
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
 
-	contractAddress := common.HexToAddress(config.GetVerifier().PlasmaContractAddress)
-	query := ethereum.FilterQuery{
-		Addresses: []common.Address{contractAddress},
+	maxBlock, err := client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	contractAddress := common.HexToAddress("0xa86a2c6B81C22d25D8EBAf8cE52895F46A263348")
+	query := ethereum.FilterQuery{
+		FromBlock: big.NewInt(int64(currentBlock)),
+		ToBlock:   big.NewInt(int64(checker(currentBlock, maxBlock.Number.Uint64()))),
+		Addresses: []common.Address{
+			contractAddress,
+		},
+	}
+
+	logs, err := client.FilterLogs(context.Background(), query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	contractAbi, err := abi.JSON(strings.NewReader(string(store.StoreABI)))
 	if err != nil {
-
-	}
-	logs := make(chan types.Log)
-	sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
-	if err != nil {
-
+		log.Fatal(err)
 	}
 
+
+	Sig := []byte("CoinDeposited(address,uint256)")
+	SigHash := crypto.Keccak256Hash(Sig)
+
+	for _, vLog := range logs {
+		switch vLog.Topics[0].Hex() {
+		case SigHash.Hex():
+			var depositEvent EventCoinDeposited
+			err := contractAbi.Unpack(&depositEvent, "CoinDeposited", vLog.Data)
+			if err != nil {
+				log.Fatal(err)
+			}
+			depositEvent.Who = common.HexToAddress(vLog.Topics[1].Hex())
+			depositEvent.BlockNumber = vLog.BlockNumber
+			PutEventsToGroup(depositEvent)
+
+		}
+	}
+
+	if currentBlock <= maxBlock.Number.Uint64() + 1 {
+		SetLastBlock(currentBlock + 1)
+	}
+
+	if currentBlock >= maxBlock.Number.Uint64() {
+		return false
+	}
+	return true
+}
+
+func SetLastBlock(v uint64) {
+	currentBlock = v
+}
+
+func checker(current, final uint64) uint64 {
+	if current + 1 <= final {
+		current = current+1
+	} else {
+		delta := final - current
+		current = current + delta
+	}
+	return current
+}
+
+
+func PutEventsToGroup(e EventCoinDeposited) {
+	eventGroup = append(eventGroup, e)
+}
+
+func ShowGroup() {
+	for i := range eventGroup {
+		fmt.Printf("BlockNumber: %d\n", eventGroup[i].BlockNumber)
+		fmt.Printf("Amount: %s\n", eventGroup[i].Amount.String())
+		fmt.Printf("Who: %s\n", eventGroup[i].Who.String())
+	}
+}
+func EventListener() {
 	for {
-		select {
-		case err := <-sub.Err():
-			log.Fatal(err)
-		case vLog := <-logs:
-			Sig := []byte("CoinDeposited(address,uint256)")
-			SigHash := crypto.Keccak256Hash(Sig)
-			switch vLog.Topics[0].Hex() {
-			case SigHash.Hex():
-				event := struct {
-					Who    common.Address
-					Amount *big.Int
-				}{}
-				err := contractAbi.Unpack(&event, "CoinDeposited", vLog.Data)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-
-				// fmt.Println("------------------------------")
-				// fmt.Println("Triggered event: CoinDeposited")
-
-				// fmt.Print("Who: ")
-				// fmt.Println(common.HexToAddress(vLog.Topics[1].Hex()).String())
-
-				// fmt.Print("Sum in wei: ")
-				// fmt.Println(event.Amount.String())
-				// fmt.Println("------------------------------")
-
+		for {
+			if !GetEvent() {
+				break
 			}
 		}
 	}
 }
+var round int
+
+func EventShow() {
+	for {
+		fmt.Println("Show round " + strconv.Itoa(round))
+		round++
+		ShowGroup()
+		time.Sleep(time.Second * 15)
+	}
+}
+
