f2d2599a0dbf1428d2bb6441870d277a25ddff49
Anton Bukov <k06aaa@gmail.com>
2018-12-08 16:54:59 +0800

    Update Bankex Plasma

--- contracts/contracts/BankexPlasma.sol
+++ contracts/contracts/BankexPlasma.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.24;
+pragma experimental ABIEncoderV2;
 
 import { PlasmaBlocks } from "./PlasmaBlocks.sol";
+import { PlasmaAssets } from "./PlasmaAssets.sol";
 
 
-
-
-contract BankexPlasma is PlasmaBlocks {
+contract BankexPlasma is PlasmaBlocks, PlasmaAssets {
 
 }
